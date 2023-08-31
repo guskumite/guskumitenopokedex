ff04e6a1357a27185572632d4ab9c8cc62f5e710
guskumite <gmolanoruiz2000@gmail.com>
2023-08-31 00:47:30 -0500

    version 1.0.1

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -19,6 +19,17 @@ const PokemonDetail = () => {
   }, []);
 
   console.log(pokemonData);
+
+  let ability1 = "";
+  let ability2 = "";
+  ability1 = pokemonData?.abilities[0].ability.name;
+  if (pokemonData?.abilities.length > 1)
+    ability2 = pokemonData?.abilities[1].ability.name;
+  let type1 = "";
+  let type2 = "";
+  type1 = pokemonData?.types[0];
+  if (pokemonData?.types.length > 1) type2 = pokemonData?.types[1];
+
   return (
     <main className="flex justify-center items-center w-full">
       <div className="flex flex-col w-full">
@@ -38,11 +49,75 @@ const PokemonDetail = () => {
               </div>
             </header>
             <section className="">
-              <div className="flex flex-col text-center">
-                <span className="text-xl font-bold">#{pokemonData?.id}</span>
-                <span className="text-3xl font-bold">{pokemonData?.name}</span>
+              <div className="flex flex-col text-center relative">
+                <span className="absolute left-1/2 -translate-x-1/2 top-5 text-xl font-bold border-2 w-16 border-blue-200">
+                  #{pokemonData?.id}
+                </span>
+                <div className="relative flex flex-row">
+                  <span className="absolute border-t-2 top-[85px] border-gray-200 w-[20vw]"></span>
+                  <span className="absolute top-10 left-1/2 translate-y-3/4 -translate-x-1/2 text-3xl font-bold">
+                    {pokemonData?.name}
+                  </span>
+                  <span className="absolute border-t-2 top-[85px] right-[5px] border-gray-200 w-[20vw]"></span>
+                </div>
+                <div className="relative flex flex-row">
+                  <div className="absolute flex-flex-col w-[20vw]">
+                    <div className="relative top-[120px] left-[10vw]">
+                      Weight:
+                    </div>
+                    <div className="relative top-[130px] left-[10vw]">
+                      {pokemonData?.weight}
+                    </div>
+                  </div>
+                  <div className="absolute flex flex-col w-[20vw]">
+                    <div className="relative top-[120px] left-[30vw]">
+                      Height:
+                    </div>
+                    <div className="relative top-[130px] left-[30vw]">
+                      {pokemonData?.height}
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute -bottom-60  flex flex-row">
+                  <span className="relative font-bold text-xl left-[5vw]">
+                    Type
+                  </span>
+                  <span
+                    className={`w-[10vw] relative font-bold text-md -left-10 top-[50px] border-2
+                  ${bgStylePokemonType[pokemonData?.types[0]]}`}
+                  >
+                    {" "}
+                    {type1}{" "}
+                  </span>
+                  <span
+                    className={`w-[10vw] relative font-bold text-md top-[50px] border-2
+                  ${bgStylePokemonType[pokemonData?.types[0]]}`}
+                  >
+                    {" "}
+                    {type2}{" "}
+                  </span>
+                  <span className="relative font-bold text-xl left-[20vw]">
+                    Abilities
+                  </span>
+                  <span
+                    className={`-right-10 w-[10vw] relative font-bold text-md top-[50px] border-2
+                  ${bgStylePokemonType[pokemonData?.types[0]]}`}
+                  >
+                    {" "}
+                    {ability1}{" "}
+                  </span>
+                  <span
+                    className={`-right-20 w-[10vw] relative font-bold text-md top-[50px] border-2
+                  ${bgStylePokemonType[pokemonData?.types[0]]}`}
+                  >
+                    {" "}
+                    {ability2}{" "}
+                  </span>
+                </div>
               </div>
-              <StatBarList stats={pokemonData?.stats} />
+              <div className="mt-[330px]">
+                <StatBarList stats={pokemonData?.stats} />
+              </div>
             </section>
           </article>
         </div>
