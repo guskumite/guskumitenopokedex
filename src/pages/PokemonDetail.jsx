63f8d3abee88a927d3a29e0f31ba60e9792e423c
guskumite <gmolanoruiz2000@gmail.com>
2023-08-31 07:47:02 -0500

    version 1.0.5

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -70,10 +70,10 @@ const PokemonDetail = () => {
                     </div>
                   </div>
                   <div className="absolute flex flex-col w-[20vw]">
-                    <div className="relative top-[120px] right-[2rem]">
+                    <div className="relative top-[120px] -right-[2rem]">
                       Height:
                     </div>
-                    <div className="relative top-[130px] right-[2rem]">
+                    <div className="relative top-[130px] -right-[2rem]">
                       {pokemonData?.height}
                     </div>
                   </div>
