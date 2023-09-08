9fc76d5fde9694887fbdc3470ad22bb3d05fe439
guskumite <gmolanoruiz2000@gmail.com>
2023-09-07 20:53:47 -0500

    version 1.0.32

--- src/components/layout/HeaderPokedex.jsx
+++ src/components/layout/HeaderPokedex.jsx
@@ -11,11 +11,11 @@ const HeaderPokedex = () => {
         <div className="h-14 bg-black relative">
           <div
             className="h-16 aspect-square bg-white rounded-full absolute left-3/4
-          -translate-x-1/2 -top-8 border-[8px] border-black hover:bg-red hover:left-1/2 md:invisible"
+          -translate-x-1/2 -top-8 border-[8px] border-black hover:bg-red hover:left-1/2 md:hidden"
           >
             <div
               className="h-10 aspect-square bg-[#333] rounded-full border-[5px] border-black
-              mt-[4px] ml-[4px] hover:bg-white md:invisible"
+              mt-[4px] ml-[4px] hover:bg-white md:hidden"
             ></div>
           </div>
         </div>
